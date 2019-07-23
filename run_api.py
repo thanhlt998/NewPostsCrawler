00708d3a7fee5603589017a5cc9487179818063e
thanhlt998 <thanhlt998@gmail.com>
2019-07-23 23:10:45 +0700

    Fix bind address api

--- run_api.py
+++ run_api.py
@@ -3,6 +3,7 @@ from werkzeug.serving import run_simple
 
 from kafka_producer import Producer
 from settings.url_crawler_settings import SETTINGS, NEW_URLS_TOPIC
+from settings.api_settings import BIND_ADDRESS, BIND_PORT
 from proto_message import UrlsMessage
 
 
@@ -14,19 +15,19 @@ def get_app():
         if request.headers['Content-Type'] == 'application/json':
             data = request.json
             if not data.get('urls'):
-                return "Url list should map with 'urls' field"
+                return Response("Url list should map with 'urls' field", status=500)
             elif not isinstance(data.get('urls'), list):
-                return "'urls' field should map with a list"
+                return Response("'urls' field should map with a list", status=500)
             else:
                 urls = data['urls']
                 producer.send_message(NEW_URLS_TOPIC, UrlsMessage.serialize(UrlsMessage(urls=urls)))
                 return Response(status=200)
         else:
-            return "Not expected data format."
+            return Response("Not expected data format.", status=500)
 
     return app
 
 
 if __name__ == '__main__':
     producer = Producer(bootstrap_servers=SETTINGS['BOOTSTRAP_SERVERS'])
-    run_simple('localhost', 5000, get_app(), use_reloader=True, use_debugger=True, use_evalex=True)
+    run_simple(BIND_ADDRESS, BIND_PORT, get_app(), use_reloader=True, use_debugger=True, use_evalex=True)
